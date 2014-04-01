8d3f30645a2788de75de430a84f68bdb4c958232
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-01 12:10:50 -0500

    Add user form click event

--- app/scripts/controllers/song_form.js
+++ app/scripts/controllers/song_form.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('vibeApp')
-  .controller('SongFormCtrl', function ($scope, $http) {
+  .controller('SongFormCtrl', function ($scope, $http, $location) {
 
     $scope.genre = {
         type: "",
         user_moods: []
     };
+
+    $scope.counter = 0;
+
+    $scope.show_button = true;
 
     $scope.moods = [
       { option: "Happy", valid: false },
@@ -19,17 +23,28 @@ angular.module('vibeApp')
       for (var i = 0; i < $scope.moods.length; i++) {
         if ($scope.moods[i].valid) {
           $scope.genre.user_moods.push($scope.moods[i].option)
-          // $scope.moods[i].valid = false;
+          $scope.moods[i].valid = false;
         }
       }
+      $scope.counter++
+      $scope.finish();
     };
 
+    $scope.switchView = function () {
+      $location.path('/');
+    }
 
     $scope.request = function () {
       $http.post('/songs', { text: "hello" } ).success(function(res) {
         console.log(res);
       });
     };
+
+    $scope.finish = function () {
+      if ($scope.counter > 2) {
+        $scope.show_button = false;
+      }
+    }
 
 
   });
