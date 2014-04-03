911fb05a31f441abd8c278134fbd7ff5b4a1e6ea
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-03 12:31:38 -0500

    Inital styling for mood form

--- app/scripts/controllers/mood.js
+++ app/scripts/controllers/mood.js
@@ -2,9 +2,12 @@
 
 angular.module('vibeApp')
   .controller('MoodCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.message = "Hello";
+
+    $scope.moods = [
+      { type: "Happy", icon: ":)" },
+      { type: "Chill", icon: "B|" },
+      { type: "Party", icon: "<:D" },
+      { type: "Sad", icon: ":(" }
     ];
   });
