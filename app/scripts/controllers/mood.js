110f858e5de743475dcb3af8a499d5cc772e5235
Neil Mithipati <neil.mithipati@gmail.com>
2014-04-04 13:49:14 -0500

    Add playlist check

--- app/scripts/controllers/mood.js
+++ app/scripts/controllers/mood.js
@@ -3,17 +3,12 @@
 // var users  = require('./controllers/users');
 
 angular.module('vibeApp')
-  .controller('MoodCtrl', function ($scope, $location, $http, User, Auth) {
+  .controller('MoodCtrl', function ($scope, $location, $http) {
 
     $scope.select = "form";
 
     $scope.userMood = "";
     $scope.userGenre = "";
-
-    $scope.findAuth = function () {
-      var x = Auth.currentUser();
-      console.log(x);
-    }
 
     $scope.moods = [
       { type: "happy", icon: ":)" },
@@ -23,23 +18,27 @@ angular.module('vibeApp')
     ];
 
     $scope.setMood = function (mood) {
-      // $scope.userMood = mood;
       $http.get('/moods/' + mood)
-        .success(function(data){
-          console.log(data);
-          $scope.select = "genre";
+        .success(function (data) {
+          if (!data) {
+            $scope.select = "genre";
+          }
+          else {
+            $location.path('/main/' + mood);
+          }
         })
-        .error(function(data) {
-          alert("Sorry, your vibe can't be processed at this time");
-        })
-    }
+        .error(function (data) {
+          window.alert("Sorry, your vibe can't be processed at this time");
+        });
+    };
 
     $scope.chooseMood = function () {
       $scope.select = "form";
-    }
+    };
 
     $scope.setPlayer = function (genre) {
-      $location.path('/main');
+        console.log("TIME TO PARTY!", genre);
+      // $location.path('/main');
       // $http.put('/api/users/new/genre', { genre: genre } )
       //   .success(function(data){
       //     console.log(data);
@@ -47,6 +46,6 @@ angular.module('vibeApp')
       //   .error(function(data){
       //     window.alert("Sorry, could not process your request at this moment. Please try again momentarily");
       //   });
-    }
+    };
 
   });
